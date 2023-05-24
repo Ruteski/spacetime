d513cc54b5b348981ae0b565918e4d9c678e7cc0
Ruteski <lruteski@gmail.com>
2023-05-24 20:01:41 -0300

    iniciado home mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View className="flex-1 items-center justify-center bg-zinc-950">
-      <Text className="text-4xl font-bold text-zinc-50">Hello World</Text>
-      <StatusBar style="auto" backgroundColor="#fff" />
+    <View className="flex-1 items-center justify-center bg-gray-900">
+      <Text className="font-body text-4xl font-bold text-zinc-50">
+        Rocketseat
+      </Text>
+      <StatusBar style="light" backgroundColor="#121215" />
     </View>
   )
 }
